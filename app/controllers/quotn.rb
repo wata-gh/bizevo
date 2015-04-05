d4f2aea7fb27d5fa0793bfcb73fb27ee7babbee7
wata <wata.gm@gmail.com>
2015-04-06 01:09:25 +0900

    add project detail

--- app/controllers/quotn.rb
+++ app/controllers/quotn.rb
@@ -1,12 +1,32 @@
 Bizevo::App.controllers :quotn do
 
   get :index, :with => :month do
-    @quotn = Quotation.current_month(params[:month]).joins(:project, :csc_cust).includes(:project, :csc_cust).order(:main_group_mst_blg_cd, :pcu_cd, :quotn_no)
+    @quotn = Quotation.current_month(params[:month]).where :main_group_mst_blg_cd => Belonging.business_blg.pluck(:mst_blg_cd)
     render 'quotn/index'
   end
 
   get :index, :map => 'quotn/:month/:blg_cd' do
-    @quotn = Quotation.current_month(params[:month]).joins(:project, :csc_cust).includes(:project, :csc_cust).where(:main_group_mst_blg_cd => params[:blg_cd])
+    @quotn = Quotation.current_month(params[:month]).where(:main_group_mst_blg_cd => params[:blg_cd])
     render 'quotn/index'
+  end
+
+  get :detail, :map => 'quotn/detail/:quotn_no/:quotn_ver_no' do
+    @quotn = Quotation.find_by :quotn_no => params[:quotn_no], :quotn_ver_no => params[:quotn_ver_no]
+    halt 404 unless @quotn
+    @reports = ProjectReport.where(:quotn_no => @quotn.quotn_no).order(:created_at => :desc).page(params[:page]).per(5)
+    render 'quotn/detail'
+  end
+
+  get :report, :map => 'quotn/detail/:quotn_no/:quotn_ver_no/report' do
+    @quotn = Quotation.find_by :quotn_no => params[:quotn_no], :quotn_ver_no => params[:quotn_ver_no]
+    halt 404 unless @quotn
+    @reports = ProjectReport.where(:quotn_no => @quotn.quotn_no).order(:created_at => :desc).page(params[:page]).per(5)
+    render 'quotn/report', :layout => false
+  end
+
+  get :report, :map => 'quotn/detail/:quotn_no/:quotn_ver_no/log' do
+    @quotn = Quotation.find_by :quotn_no => params[:quotn_no], :quotn_ver_no => params[:quotn_ver_no]
+    halt 404 unless @quotn
+    'この課題に関する作業ログはまだありません。'
   end
 end
--- models/quotation.rb
+++ models/quotation.rb
@@ -14,6 +14,19 @@ class Quotation < ActiveRecord::Base
 
   # enums
   enum cntrct_tp: {lump_sum: '1', mandate: '2', worker_dispatch: '3', maintenance: '4'}
+  enum order_stat: {
+    not_yet_started:                 '20',
+    lost_before_start:               '29',
+    expected_start:                  '30',
+    preceding_start:                 '35',
+    purchase_order_received:         '40',
+    completion_certificate_received: '45',
+    invoices_issued:                 '50',
+    payment_confirmed:               '60',
+    lost:                            '80',
+    refuse:                          '90',
+    exit_on_the_way:                 '95',
+  }
 
   # scopes
   scope :current_month, ->(m) {
@@ -23,7 +36,9 @@ class Quotation < ActiveRecord::Base
     or (quotation.start_date > '#{m}' and quotation.end_date > '#{m}99')
     or (quotation.start_date < '#{m}' and quotation.end_date > '#{m}99')
    EOS
-   ).order([:main_group_mst_blg_cd, :pcu_cd, :quotn_no])
+   ).includes(:project, :csc_cust, :assign_pmbrs)
+    .references(:project, :csc_cust, :assign_pmbrs)
+    .order(:main_group_mst_blg_cd, :pcu_cd, :quotn_no)
   }
 
   def cntrct_tp_color
@@ -32,5 +47,32 @@ class Quotation < ActiveRecord::Base
     return 'yellow' if self.worker_dispatch?
     return 'orange' if self.maintenance?
     'black'
+  end
+
+  def order_stat_color
+    return 'blue' if self.expected_start? || self.preceding_start? || self.purchase_order_received?
+    return 'green' if self.purchase_order_received? || self.completion_certificate_received? || self.invoices_issued? || self.payment_confirmed?
+    return 'red' if self.exit_on_the_way?
+    return 'black' if self.lost?
+    ''
+  end
+
+  def man_hour_mm month
+    assign_pmbrs.inject(0) do |s, m|
+      pmh = m.assign_pmh.find_by :setup_date_ym => month
+      s + (pmh.present? ? pmh.man_hour_mm : 0)
+    end
+  end
+
+  def order_stat_i18n
+    Quotation.translate_enum_label self.class, 'order_stat', self.order_stat
+  end
+
+  def cntrct_tp_i18n
+    Quotation.translate_enum_label self.class, 'cntrct_tp', self.cntrct_tp
+  end
+
+  def self.translate_enum_label(klass, attr_name, enum_label)
+    ::I18n.t("enums.#{klass.to_s.underscore}.#{attr_name}.#{enum_label}", default: enum_label)
   end
 end
