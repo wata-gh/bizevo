31c1f54b6e46af118eeb280350f529ff96b5a1cf
tamai <jyunichi.k@gmail.com>
2015-04-26 16:46:51 +0900

    top のデザイン変更 タグページを作成 記事検索のSQLを innner join から outer joinに変更。 タグがなくても出るように。 タグを必須にしたら不要になるけど。

--- models/article.rb
+++ models/article.rb
@@ -1,6 +1,10 @@
 class Article < ActiveRecord::Base
   after_save :save_article_tags
 
+  def mine? id
+    self.user_id == id
+  end
+
   def get_created_at
     self.created_at.strftime '%Y/%m/%d'
   end
@@ -21,5 +25,5 @@ class Article < ActiveRecord::Base
   validates_presence_of :title
   validates_presence_of :article
 
-  paginates_per 10
+  paginates_per 5 
 end
