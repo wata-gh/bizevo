ee3d930cbefaff25b6f4896d0744ddc65a78e2f0
wata <wata.gm@gmail.com>
2015-04-29 01:38:49 +0900

    add with login

--- db/schema.rb
+++ db/schema.rb
@@ -11,44 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 8) do
+ActiveRecord::Schema.define(version: 9) do
 
-  create_table "accounts", force: true do |t|
-    t.string   "name"
-    t.string   "surname"
-    t.string   "email"
-    t.string   "crypted_password"
-    t.string   "role"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+# Could not dump table "accounts" because of following NoMethodError
+#   undefined method `virtual?' for #<ActiveRecord::ConnectionAdapters::Mysql2Adapter::Column:0x007f356dcbc308>
 
-  create_table "article_tags", id: false, force: true do |t|
-    t.integer  "article_id"
-    t.string   "tag"
-    t.integer  "delete_flg"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+# Could not dump table "article_tags" because of following NoMethodError
+#   undefined method `virtual?' for #<ActiveRecord::ConnectionAdapters::Mysql2Adapter::Column:0x007f356dcfbf58>
 
-  add_index "article_tags", ["article_id", "tag"], name: "index_article_tags_on_article_id_and_tag", unique: true, using: :btree
+# Could not dump table "articles" because of following NoMethodError
+#   undefined method `virtual?' for #<ActiveRecord::ConnectionAdapters::Mysql2Adapter::Column:0x007f356e011878>
 
-  create_table "articles", force: true do |t|
-    t.integer  "user_id"
-    t.string   "title"
-    t.text     "article"
-    t.integer  "delete_flg"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+# Could not dump table "project_reports" because of following NoMethodError
+#   undefined method `virtual?' for #<ActiveRecord::ConnectionAdapters::Mysql2Adapter::Column:0x007f356e0cfd50>
 
-  create_table "tags", id: false, force: true do |t|
-    t.string   "tag"
-    t.integer  "delete_flg"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "tags", ["tag"], name: "index_tags_on_tag", unique: true, using: :btree
+# Could not dump table "tags" because of following NoMethodError
+#   undefined method `virtual?' for #<ActiveRecord::ConnectionAdapters::Mysql2Adapter::Column:0x007f356e329f88>
 
 end
