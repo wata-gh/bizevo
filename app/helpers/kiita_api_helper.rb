b8733c95d96e738cc040fd55463e2fc9cb28eddc
tamai <jyunichi.k@gmail.com>
2015-05-07 17:52:18 +0900

    不要な json のハンドラーを削除

--- app/helpers/kiita_api_helper.rb
+++ app/helpers/kiita_api_helper.rb
@@ -3,21 +3,7 @@ require 'json'
 module Bizevo
   class App
     module KiitaApiHelper
-      ##
-      # エラーレスポンス返却
-      #
-      def err_res v = {}, root_opt = {}
-        content_type 'application/json; charset=utf-8'
-        {is_success: 0}.merge(root_opt).merge({error: v}).to_json
-      end
-
-      ##
-      # 正常レスポンス返却
-      #
-      def suc_res v = {}, root_opt = {}
-        content_type 'application/json; charset=utf-8'
-        {is_success: 1}.merge(root_opt).merge({results: v}).to_json
-      end
+      
     end
 
     helpers KiitaApiHelper
