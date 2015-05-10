9b2194322bc6bcc0ca435f552e52fe3e2f485d43
tamai <jyunichi.k@gmail.com>
2015-05-10 19:20:32 +0900

    いいね の仕組みを追加 誰でも無限にいいねができます。

--- app/helpers/kiita_api_helper.rb
+++ app/helpers/kiita_api_helper.rb
@@ -1,9 +1,15 @@
-require 'json'
-
 module Bizevo
   class App
     module KiitaApiHelper
-      
+
+      def increment_like article_id
+        article = Article.find_by_id article_id
+        return nil unless article
+        article.lock!
+        article.like = article.like + 1
+        return article if article.save
+        nil
+      end
     end
 
     helpers KiitaApiHelper
--- spec/app/helpers/kiita_api_helper_spec.rb
+++ spec/app/helpers/kiita_api_helper_spec.rb
@@ -1,13 +1,23 @@
 require 'spec_helper'
 
 RSpec.describe "Bizevo::App::KiitaApiHelper" do
-  pending "add some examples to (or delete) #{__FILE__}" do
+  before :all do
+    @articles = FactoryGirl.create :article
+  end
+
+  after :all do
+    @articles.destroy rescue nil
+  end
+
+  describe "#{__FILE__}" do
     let(:helpers){ Class.new }
     before { helpers.extend Bizevo::App::KiitaApiHelper }
     subject { helpers }
 
-    it "should return nil" do
-      expect(subject.foo).to be_nil
+    describe "increment_like のテスト" do
+      it "like が増えること" do
+        expect(helpers.increment_like(1).like).to eq 1
+      end
     end
   end
 end
