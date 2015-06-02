2089deb63ed1e043c9fd24ecf8f3c68d4b19b99c
tsuka611 <tsuka0611@gmail.com>
2015-06-03 00:04:34 +0900

    modify api url.

--- spa/tea/src/components/resources/party/user.resource.js
+++ spa/tea/src/components/resources/party/user.resource.js
@@ -2,13 +2,13 @@
 
 angular.module('tea')
   .factory('User', function ($resource) {
-    return $resource('api/tea/user/:id',
+    return $resource('api/user/:id',
         {
           id: '@id',
         },{
           me: {
             method: 'GET',
-            url: 'api/tea/user/me',
+            url: 'api/user/me',
             isArray: false,
             params: {
             },
