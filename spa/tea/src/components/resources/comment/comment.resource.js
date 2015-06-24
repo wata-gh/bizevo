248b2e9db3ecc61daaa250fb734ca9e588c1a0c6
Yusuke Ohtsuka <tsuka0611@gmail.com>
2015-06-24 12:13:37 +0900

    comment resource created.

---
+++ spa/tea/src/components/resources/comment/comment.resource.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('tea')
+  .factory('Comment', function (teaResource) {
+    return teaResource('api/comment/:id',
+        {
+          id: '@id',
+        },{
+          newComment: {
+            method: 'POST',
+            url: 'api/comment/:parentId',
+            isArray: false,
+            params: {
+              parentId: '@parentId',
+            },
+          },
+          edit: {
+            method: 'PUT',
+            url: 'api/comment/:id',
+            isArray: false,
+            params: {
+              parentId: '@id',
+            },
+          },
+        });
+  });
